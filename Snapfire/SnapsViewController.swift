9249a641f92d752178aad33acf400c250ebf464d
Thomas Harper <thomasbentkowski@gmail.com>
2016-11-08 18:32:04 +0100

    Adding of SnapsViewController and logoutButton. Dismissing SnapsViewController when user taps on logout

---
+++ Snapfire/SnapsViewController.swift
@@ -0,0 +1,28 @@
+//
+//  SnapsViewController.swift
+//  Snapfire
+//
+//  Created by Thomas Bentkowski on 08/11/2016.
+//  Copyright © 2016 Thomas Bentkowski. All rights reserved.
+//
+
+import UIKit
+
+class SnapsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func logoutTapped(_ sender: AnyObject) {
+        dismiss(animated: true, completion: nil)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
