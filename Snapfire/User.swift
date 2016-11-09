6ffafe0f5cd9994a5e6a7519d657fe0554273288
Thomas Harper <thomasbentkowski@gmail.com>
2016-11-09 18:27:52 +0100

    User class created and organizing file system : ViewControllers and Models

---
+++ Snapfire/User.swift
@@ -0,0 +1,18 @@
+//
+//  User.swift
+//  Snapfire
+//
+//  Created by Thomas Bentkowski on 09/11/2016.
+//  Copyright © 2016 Thomas Bentkowski. All rights reserved.
+//
+
+import Foundation
+
+class User {
+    
+    /////////////////// OUTLETS ////////////////////
+    ///////////////// PROPERTIES ///////////////////
+    var email : String?
+    var uid : String?
+    
+}
