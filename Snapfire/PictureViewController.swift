089a1b146bb0cd1c2cce4037a67d8917ee2bb688
Thomas Harper <thomasbentkowski@gmail.com>
2016-11-09 14:45:37 +0100

    Setting up the UIImagePicker

--- Snapfire/PictureViewController.swift
+++ Snapfire/PictureViewController.swift
@@ -8,21 +8,39 @@
 
 import UIKit
 
-class PictureViewController: UIViewController {
+class PictureViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     /////////////////// OUTLETS ////////////////////
     @IBOutlet weak var snapImage: UIImageView!
     @IBOutlet weak var descriptionTextField: UITextField!
     @IBOutlet weak var nextButton: UIButton!
     ///////////////// PROPERTIES ///////////////////
+    var imagePicker = UIImagePickerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        imagePicker.delegate = self
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        // Where do we want our photos from and we don't want edited photos
+        imagePicker.sourceType = .savedPhotosAlbum
+        imagePicker.allowsEditing = false
+        
+        // Let's take the image from the picker
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        snapImage.image = image
+        
+        // When the user picks a picture, disabling the background color of the UIImageView
+        snapImage.backgroundColor = UIColor.clear
+        
+        // Closing the image picker
+        imagePicker.dismiss(animated: true, completion: nil)
     }
 
     @IBAction func cameraTapped(_ sender: Any) {
+        present(imagePicker, animated: true, completion: nil)
     }
     @IBAction func nextTapped(_ sender: Any) {
     }
