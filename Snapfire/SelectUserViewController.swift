e5a35351a303929a4f7a6174f6a8a1309f6526d5
Thomas Harper <thomasbentkowski@gmail.com>
2016-11-09 18:01:02 +0100

    Setting up the tableView of SelectUserViewController which is now connected to the storyboard

---
+++ Snapfire/SelectUserViewController.swift
@@ -0,0 +1,38 @@
+//
+//  SelectUserViewController.swift
+//  Snapfire
+//
+//  Created by Thomas Bentkowski on 09/11/2016.
+//  Copyright © 2016 Thomas Bentkowski. All rights reserved.
+//
+
+import UIKit
+
+class SelectUserViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    /////////////////// OUTLETS ////////////////////
+    @IBOutlet weak var tableView: UITableView!
+    ///////////////// PROPERTIES ///////////////////
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        return cell
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
