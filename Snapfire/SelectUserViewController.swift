fa41a76b12529e77edb8df42a6ee47bc4c022df2
Thomas Harper <thomasbentkowski@gmail.com>
2016-11-09 19:13:56 +0100

    Pulling data from Firebase in order to display users email to the recipients list

--- Snapfire/SelectUserViewController.swift
+++ Snapfire/SelectUserViewController.swift
@@ -7,26 +7,43 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
 class SelectUserViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     /////////////////// OUTLETS ////////////////////
     @IBOutlet weak var tableView: UITableView!
     ///////////////// PROPERTIES ///////////////////
+    var users : [User] = []
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.tableView.delegate = self
         self.tableView.dataSource = self
+        
+        FIRDatabase.database().reference().child("users").observe(FIRDataEventType.childAdded, with: {(snapshot) in
+            let user = User()
+            let snapshotDictionnary = snapshot.value as? NSDictionary
+            user.email = snapshotDictionnary!["email"] as? String
+            user.uid = snapshot.key
+            
+            self.users.append(user)
+            self.tableView.reloadData()
+        })
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 6
+        return users.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
+        let user = users[indexPath.row]
+        
+        cell.textLabel?.text = user.email
+        
         return cell
     }
 
