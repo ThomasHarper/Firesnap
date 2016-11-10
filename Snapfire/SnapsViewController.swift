1121597bfcbce64f9259a773445ee33855fd66b0
Thomas Harper <thomasbentkowski@gmail.com>
2016-11-10 16:29:21 +0100

    Added the snaps that the user received to its snaps list

--- Snapfire/SnapsViewController.swift
+++ Snapfire/SnapsViewController.swift
@@ -7,16 +7,62 @@
 //
 
 import UIKit
+import FirebaseDatabase
+import FirebaseAuth
 
-class SnapsViewController: UIViewController {
+class SnapsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    /////////////////// OUTLETS ////////////////////
+    @IBOutlet weak var tableView: UITableView!
+    ///////////////// PROPERTIES ///////////////////
+    var snaps : [Snap] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        // Pulling the data from Firebase Database in order to display the list of snaps that the user received
+        FIRDatabase.database().reference().child("users").child((FIRAuth.auth()?.currentUser!.uid)!).child("snaps").observe(FIRDataEventType.childAdded, with: {(snapshot) in
+            let snap = Snap()
+            let snapshotDictionnary = snapshot.value as? NSDictionary
+            snap.imageURL = (snapshotDictionnary!["imageURL"] as? String)!
+            snap.from = (snapshotDictionnary!["from"] as? String)!
+            snap.description = (snapshotDictionnary!["description"] as? String)!
+            
+            self.snaps.append(snap)
+            self.tableView.reloadData()
+            })
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return snaps.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let snap = snaps[indexPath.row]
+        cell.textLabel?.text = snap.from
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let snap = snaps[indexPath.row]
+        // Let's go to the snap that the user selected
+        performSegue(withIdentifier: "showsnap", sender: snap)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Sending the snap to the next view controller (ViewSnapViewController)
+        if segue.identifier == "showsnap" {
+            let nextVC = segue.destination as! ViewSnapViewController
+            nextVC.snap = sender as! Snap
+        }
     }
 
     @IBAction func logoutTapped(_ sender: AnyObject) {
+        // When the user taps on logout we dismiss the current view controller 
+        // and he goes back to signin
         dismiss(animated: true, completion: nil)
     }
 
