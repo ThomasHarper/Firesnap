42bf6418a2853bbe90c29f76f62dde15ccab2dfa
Thomas Harper <thomasbentkowski@gmail.com>
2016-11-10 18:29:00 +0100

    Showing "No snaps" if the user don't have one

--- Snapfire/SnapsViewController.swift
+++ Snapfire/SnapsViewController.swift
@@ -51,13 +51,22 @@ class SnapsViewController: UIViewController, UITableViewDelegate, UITableViewDat
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return snaps.count
+        if snaps.count == 0 {
+            return 1
+        } else {
+            return snaps.count
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        let snap = snaps[indexPath.row]
-        cell.textLabel?.text = snap.from
+        if snaps.count == 0 {
+            cell.textLabel?.text = "YOU HAVE NO SNAPS 😭"
+            cell.textLabel?.textAlignment = NSTextAlignment.center
+        } else {
+            let snap = snaps[indexPath.row]
+            cell.textLabel?.text = snap.from
+        }
         return cell
     }
     
